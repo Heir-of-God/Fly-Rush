edf8a50b7f7791fec346b20d9dfdc60f1d461a5f
Heir-of-God <140493860+Heir-of-God@users.noreply.github.com>
2024-06-06 11:31:43 +0300

    Added: described enemy's moving

--- constants.py
+++ constants.py
@@ -14,3 +14,8 @@ BACKGROUND_SPEED = 2  # px per frame
 PLAYER_SPEED_Y: int = 8  # px per frame
 PLAYER_SPEED_X_RIGHT: int = 10  # px per frame
 PLAYER_SPEED_X_LEFT: int = 7  # px per frame
+
+# Enemies
+ENEMY_SPEED_X = 5  # px per frame
+ENEMY_SPEED_Y = 1  # px per frame
+ENEMY_DELTA_Y: tuple[int, int] = (90, 280)  # (minimum, maximum) delta y on which enemy's plane can deviate
--- planes.py
+++ planes.py
@@ -2,7 +2,16 @@
 
 from random import choice, randint
 import pygame as pg
-from constants import GAME_SCREEN_HEIGHT, GAME_SCREEN_WIDTH, PLAYER_SPEED_X_RIGHT, PLAYER_SPEED_X_LEFT, PLAYER_SPEED_Y
+from constants import (
+    GAME_SCREEN_HEIGHT,
+    GAME_SCREEN_WIDTH,
+    PLAYER_SPEED_X_RIGHT,
+    PLAYER_SPEED_X_LEFT,
+    PLAYER_SPEED_Y,
+    ENEMY_SPEED_X,
+    ENEMY_SPEED_Y,
+    ENEMY_DELTA_Y,
+)
 
 
 class Plane(pg.sprite.Sprite):
@@ -20,6 +29,11 @@ class Plane(pg.sprite.Sprite):
     def bullet_reload(self) -> None:
         if self.reload_time != 0:
             self.reload_time -= 1
+
+    def update_collide_rect(self) -> None:
+        self.collide_rect.center = self.rect.centerx, self.rect.centery + (
+            self.rect.width * 0.08 if self.type == 1 else self.rect.width * 0.04
+        )
 
 
 class PlayerPlane(Plane):
@@ -42,9 +56,7 @@ class PlayerPlane(Plane):
             self.rect.right = min(self.rect.right + PLAYER_SPEED_X_RIGHT, GAME_SCREEN_WIDTH)
         if keys[pg.K_a]:
             self.rect.left = max(self.rect.left - PLAYER_SPEED_X_LEFT, 0)
-        self.collide_rect.center = self.rect.centerx, self.rect.centery + (
-            self.rect.width * 0.08 if self.type == 1 else self.rect.width * 0.04
-        )
+        self.update_collide_rect()
 
     def update(self) -> None:
         self.bullet_reload()
@@ -55,11 +67,12 @@ class EnemyPlane(Plane):
     def __init__(self) -> None:
         super().__init__()
         self.type: int = randint(1, 2)
-        self.image: pg.Surface = pg.transform.rotozoom(
-            pg.image.load(f"assets/graphics/planes/{choice(['red', 'blue', 'green', 'yellow'])}{self.type}.png"),
-            0,
-            0.15,
-        ).convert_alpha()
+        self.image: pg.Surface = pg.image.load(
+            f"assets/graphics/planes/{choice(['red', 'blue', 'green', 'yellow'])}{self.type}.png"
+        )
+        self.image = pg.transform.rotozoom(self.image, 0, 0.15).convert_alpha()
+        self.image = pg.transform.flip(self.image, True, False)
+
         self.image_width: int = self.image.get_width()
         self.rect: pg.Rect = self.image.get_rect(
             topleft=(
@@ -68,4 +81,28 @@ class EnemyPlane(Plane):
             )
         )
         self.right_target_x: int = randint(GAME_SCREEN_WIDTH - self.image_width * 3, GAME_SCREEN_WIDTH)
+        self.speed_y: int = choice([ENEMY_SPEED_Y, -ENEMY_SPEED_Y])
+        self.pos_y_delta: int = randint(ENEMY_DELTA_Y[0], ENEMY_DELTA_Y[1])
+        self.start_coor_y_top: int = self.rect.top
         self.create_collide_rect()
+
+    def move(self) -> None:
+        # if enemy hasn't reached its target yet
+        if self.rect.right >= self.right_target_x:
+            self.rect.x -= ENEMY_SPEED_X
+        else:
+            self.rect.y += self.speed_y
+            if self.speed_y > 0:  # Enemy is going down
+                if self.rect.bottom >= GAME_SCREEN_HEIGHT:
+                    self.rect.bottom = GAME_SCREEN_HEIGHT
+                    self.speed_y *= -1
+                elif self.rect.bottom >= self.start_coor_y_top + self.rect.height + self.pos_y_delta:
+                    self.speed_y *= -1
+
+            else:  # enemy is going up
+                if self.rect.top <= 0:
+                    self.rect.top = 0
+                    self.speed_y *= -1
+                elif self.rect.top <= self.start_coor_y_top - self.pos_y_delta:
+                    self.speed_y *= -1
+        self.update_collide_rect()
