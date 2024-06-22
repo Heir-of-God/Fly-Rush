c3dd4e7ee5f3e3073bb802ba6f078831a00bf574
Heir-of-God <140493860+Heir-of-God@users.noreply.github.com>
2024-06-22 11:51:15 +0300

    Extended: GameSaveLoadSystem class

--- save_load_system.py
+++ save_load_system.py
@@ -38,3 +38,33 @@ class GameSaveLoadSystem:
             data = pc.load(data_file)
 
         return data
+
+    def load_game_data(self, files_to_load_dict: dict[str, Any]) -> list[Any]:
+        """Method to load all data from all files in one dict
+
+        Args:
+            files_to_load_dict (dict[str, Any]): dictionary which contains key-value pairs where key is the name for the file
+            and value is the default value which will be returned in case file is empty.
+
+        Returns:
+            dict: key is the name of the file and the value is the value assigned to it
+        """
+        return_data: list[Any] = files_to_load_dict.copy()
+        for file_name in files_to_load_dict:
+            file_path: str = path.join(
+                self.absolute_folder_path, self.data_folder_name, file_name + self.file_extension
+            )
+            if path.exists(file_path):
+                return_data[file_name] = self.load_data(file_name)
+
+        return return_data
+
+    def save_game_data(self, files_to_save_dict) -> None:
+        """Method to save all data from game with 1 dict
+
+        Args:
+            files_to_save_dict (dict[str, Any]): dictionary which contains key-value pairs where key is the name for the file
+            and value is the value to write in this file.
+        """
+        for file_name, data in files_to_save_dict.items():
+            self.save_data_to_file(file_name, data)
